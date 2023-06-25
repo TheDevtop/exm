be717ac493282e7bf7883d336701ed0f4dabe84d
Thijs Haker <theage2001@gmail.com>
2023-06-25 21:58:27 +0200

    Moved ui code around.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react'
 import * as Material from '@mui/material/'
 import MenuBar from './MenuBar'
+import axios from 'axios';
 import * as Lib from './lib'
 
 export default function App() {
   const [rex, setRex] = React.useState('');
   const [obj, setObj] = React.useState('');
 
+  const [, setHits] = React.useState(0);
+  const [results, setResults] = React.useState([""]);
+
   function clicked() {
-    /*
-    Use the lib package to communicate with EXM.
-    Print the results as a table in the result card.
-    */
-   const rf = Lib.Search(rex, obj);
-   console.log(rf);
+    const bodyForm = {
+      Object: obj,
+      Regex: rex,
+    };
+
+    axios.post(Lib.addr, bodyForm).then((resp) => {
+      let rf = resp.data as Lib.ResultForm;
+      if (rf.Error == "") {
+        setHits(rf.Count);
+        setResults(rf.Result);
+        return;
+      }
+      console.warn(rf.Error);
+    }).catch((err) => {
+      console.error(err);
+    });
+
+    console.log(results);
   }
 
   return (
@@ -22,17 +38,17 @@ export default function App() {
       <Material.Grid container spacing={4}>
         <Material.Grid item xs={6}>
           <Material.Card><Material.CardContent>
-          <Material.TextField margin="dense" onChange={r => setRex(r.target.value)} id="outlined-basic" label="Regex" variant="outlined" />
-          <Material.TextField margin="dense" onChange={o => setObj(o.target.value)} id="outlined-basic" label="Object" variant="outlined" />
+            <Material.TextField margin="dense" onChange={r => setRex(r.target.value)} id="outlined-basic" label="Regex" variant="outlined" />
+            <Material.TextField margin="dense" onChange={o => setObj(o.target.value)} id="outlined-basic" label="Object" variant="outlined" />
           </Material.CardContent>
-          <Material.CardActions>
-            <Material.Button onClick={clicked} variant="contained">Search</Material.Button>
-          </Material.CardActions>
+            <Material.CardActions>
+              <Material.Button onClick={clicked} variant="contained">Search</Material.Button>
+            </Material.CardActions>
           </Material.Card>
         </Material.Grid>
         <Material.Grid item xs={6}>
           <Material.Card><Material.CardContent>
-              Results
+            Results
           </Material.CardContent></Material.Card>
         </Material.Grid>
       </Material.Grid>
--- ui/src/lib.ts
+++ ui/src/lib.ts
@@ -1,25 +1,7 @@
-import axios from 'axios';
-
-const addr = "http://exm.some.network:1800/search/object";
+export const addr = "http://0.0.0.0:1800/search/object";
 
 export class ResultForm {
     Error: string = "";
     Count: number = 0;
     Result: string[] = [];
-}
-
-export function Search(r: string, o: string): ResultForm {
-    const bodyForm = {
-        Object: o,
-        Regex: r
-    };
-
-    axios.post(addr, bodyForm).then((resp) => {
-        let rf = resp.data as ResultForm;
-        return rf;
-    }).catch((err) => {
-        console.error(err);
-    });
-
-    return new ResultForm();
 }
